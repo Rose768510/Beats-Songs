ba14e20b8fbc66d96d2924f15a838447f3d7075c
Rose768510 <roseakouvi.sossoukpe@gmail.com>
2025-03-26 14:37:17 +0000

    second commit

--- music.js
+++ music.js
@@ -1,1 +1,14 @@
 const navbar = document.querySelector('#navbar');
+
+<div class="container">
+                <img src="https://cdn.freecodecamp.org/curriculum/js-music-player/quincy-larson-album-art.jpg" alt="song cover photo">
+                <div class="text">
+                    <p class="bold"></p>
+                    <p></p>
+                    <button id="play" class="play" aria-label="Play">
+                        <svg>
+
+                        </svg>
+                    </button>
+                </div>
+            </div>
